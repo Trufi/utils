d2dd6efe26f87aac3cbdacd7dbc9b3ad7b9d44eb
Mstislav Zhivodkov <stevemyz@gmail.com>
2021-09-20 20:08:53 +0700

    Fix bug with every

--- src/map/every.ts
+++ src/map/every.ts
@@ -5,7 +5,7 @@ export function mapEvery<K, V>(m: Map<K, V>, cb: (v: V, k: K) => boolean): boole
     while (!result.done) {
         const [key, value] = result.value;
         const res = cb(value, key);
-        if (res) {
+        if (!res) {
             return false;
         }
         result = iterator.next();
